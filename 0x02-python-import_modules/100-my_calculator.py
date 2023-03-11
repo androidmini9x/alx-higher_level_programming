f4c8073ffb7c8d4afcfd82868f13c562d6dc5e48
Androidmini9x <androidmini9x@gmail.com>
2023-03-11 13:57:14 +0300

    Solve: 0x02 Bonus

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    argc = len(sys.argv)
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if argc != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    if sys.argv[2] not in ops:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
